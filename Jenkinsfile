bfb38d81c2bcfa60e4d35bcf4b314916219549f2
harshagns19 <57993831+harshagns19@users.noreply.github.com>
2019-11-20 14:20:20 +0100

    Add files via upload

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+pipeline {
+	agent any
+	
+	stages {
+	
+		stage('Build'){
+			steps {
+				sh 'mvn clean package'
+			}
+		}
+		
+		stage('Deploy'){
+			steps{
+				
+				sh 'mvn clean package'
+	}
+	
+	}
+	
+	
+}
---
+++ src/test/java/com/jenkins/cicd/cicdjenkinsexample/CicdJenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.cicd.cicdjenkinsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdJenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
